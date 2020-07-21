457751f9e41ffa4ec0cf854be08ff130e792c4cb
Pavel <firstcrowbar@gmail.com>
2020-07-21 19:40:41 +0500

    Первый коммит

---
+++ 2. Типы данных.js
@@ -0,0 +1,13 @@
+const persone = '5';        /* Строка */
+
+const bool = false;         /* Булевое значение */
+
+const obj = {               /* Объект, содержит в себе один и более элементов */
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(obj.isMarried); /* Пример обращения к одному из элементов объекта */
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp']; /* Массив */
+console.log(arr[0]);                               /* Пример обращения к первому элементу в массиве */
